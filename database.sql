c9483ba8446f6cae54f2c5c4b9c776527c40cfc9
eduardocruzpalacios <52535457+eduardocruzpalacios@users.noreply.github.com>
2022-04-28 22:10:02 +0200

    Insert mock data into database query

--- database.sql
+++ database.sql
@@ -48,3 +48,11 @@ DELETE FROM
   todos
 WHERE
   id = 1;
+
+/* mock data */
+INSERT INTO
+  todos (id, title, content, is_completed)
+VALUES
+  (NULL, 'mock 1', 'mock 1... mock 1', '0'),
+  (NULL, 'mock 2', 'mock 2... mock 2', '1'),
+  (NULL, 'mock 3', 'mock 3... mock 3', '1')
